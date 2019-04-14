3401d8a21fa9b89e8a43078952d47f8460b7a8c4
punam tiwari <punamt@outlook.com>
2019-04-13 23:13:23 -0700

    prepared to deploy

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ if (process.env.NODE_ENV === 'production'){
     app.get('*',(req,res)=> {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
 });
+}
 
 const port = process.env.PORT || 5200;
 app.listen(port,() => console.log(`server running on port ${port}`));
